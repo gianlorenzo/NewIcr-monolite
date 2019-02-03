f7604b23d386f21ce709ae4942574aec5bd5a319
gianlorenzo <gianlorenzo.didonato@gmail.com>
2019-02-03 12:03:41 +0100

    fix

--- src/main/java/it/uniroma3/icr/IcrApplication.java
+++ src/main/java/it/uniroma3/icr/IcrApplication.java
@@ -16,9 +16,18 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Primary;
 import org.springframework.orm.jpa.vendor.HibernateJpaSessionFactoryBean;
 
+import javax.net.ssl.HttpsURLConnection;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.TrustManagerFactory;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.sql.DataSource;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.security.GeneralSecurityException;
+import java.security.KeyStore;
 
 
 @SpringBootApplication
@@ -36,9 +45,33 @@ public class IcrApplication extends SpringBootServletInitializer {
     @Value("${server.context-parameters.jsPath}")
     private String jsPath;
 
+    private static String keyStoreName = "keystore.p12";
 
     public static void main(String[] args) throws Exception {
-        SpringApplication.run(IcrApplication.class, args);
+        try {
+            System.out.print("ciao");
+            ClassLoader classLoader = ClassLoader.getSystemClassLoader();
+            InputStream keyStoreInputStream = classLoader.getResourceAsStream(keyStoreName);
+            if (keyStoreInputStream == null) {
+                throw new FileNotFoundException("Could not find file named '" + keyStoreName + "' in the CLASSPATH");
+            }
+
+            //load the keystore
+            KeyStore keystore = KeyStore.getInstance(KeyStore.getDefaultType());
+            keystore.load(keyStoreInputStream, null);
+
+            //add to known keystore
+            TrustManagerFactory trustManagerFactory = TrustManagerFactory.getInstance(TrustManagerFactory.getDefaultAlgorithm());
+            trustManagerFactory.init(keystore);
+
+            //default SSL connections are initialized with the keystore above
+            TrustManager[] trustManagers = trustManagerFactory.getTrustManagers();
+            SSLContext sc = SSLContext.getInstance("SSL");
+            sc.init(null, trustManagers, null);
+            SSLContext.setDefault(sc);
+        } catch (IOException | GeneralSecurityException e) {
+            throw new RuntimeException(e);
+        }        SpringApplication.run(IcrApplication.class, args);
     }
 
     @SuppressWarnings("deprecation")
@@ -47,6 +80,7 @@ public class IcrApplication extends SpringBootServletInitializer {
     public HibernateJpaSessionFactoryBean sessionFactory() {
         return new HibernateJpaSessionFactoryBean();
     }
+
 
 
     @Bean
@@ -64,6 +98,8 @@ public class IcrApplication extends SpringBootServletInitializer {
             }
         };
     }
+
+
 
 
 
