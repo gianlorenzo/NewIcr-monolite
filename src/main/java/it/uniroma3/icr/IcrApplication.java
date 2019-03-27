1f35cf6994cb9b563ec5e85ceac0cdea2597a3f2
gianlorenzo.didonato <gianlorenzo.didonato@gmail.com>
2019-03-27 12:47:58 +0100

    fix

--- src/main/java/it/uniroma3/icr/IcrApplication.java
+++ src/main/java/it/uniroma3/icr/IcrApplication.java
@@ -48,30 +48,7 @@ public class IcrApplication extends SpringBootServletInitializer {
     private static String keyStoreName = "keystore.p12";
 
     public static void main(String[] args) throws Exception {
-        try {
-            System.out.print("ciao");
-            ClassLoader classLoader = ClassLoader.getSystemClassLoader();
-            InputStream keyStoreInputStream = classLoader.getResourceAsStream(keyStoreName);
-            if (keyStoreInputStream == null) {
-                throw new FileNotFoundException("Could not find file named '" + keyStoreName + "' in the CLASSPATH");
-            }
-
-            //load the keystore
-            KeyStore keystore = KeyStore.getInstance(KeyStore.getDefaultType());
-            keystore.load(keyStoreInputStream, null);
-
-            //add to known keystore
-            TrustManagerFactory trustManagerFactory = TrustManagerFactory.getInstance(TrustManagerFactory.getDefaultAlgorithm());
-            trustManagerFactory.init(keystore);
-
-            //default SSL connections are initialized with the keystore above
-            TrustManager[] trustManagers = trustManagerFactory.getTrustManagers();
-            SSLContext sc = SSLContext.getInstance("SSL");
-            sc.init(null, trustManagers, null);
-            SSLContext.setDefault(sc);
-        } catch (IOException | GeneralSecurityException e) {
-            throw new RuntimeException(e);
-        }        SpringApplication.run(IcrApplication.class, args);
+        SpringApplication.run(IcrApplication.class, args);
     }
 
     @SuppressWarnings("deprecation")
--- src/main/java/it/uniroma3/icr/controller/LoginController.java
+++ src/main/java/it/uniroma3/icr/controller/LoginController.java
@@ -1,72 +1,58 @@
 package it.uniroma3.icr.controller;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
-
 import it.uniroma3.icr.instagramConfig.InstagramJPService;
 import it.uniroma3.icr.instagramConfig.UserInstagram;
-import it.uniroma3.icr.model.StudentSocial;
+import it.uniroma3.icr.model.Administrator;
+import it.uniroma3.icr.model.Student;
 import it.uniroma3.icr.service.impl.AdminService;
+import it.uniroma3.icr.service.impl.StudentService;
 import it.uniroma3.icr.service.impl.StudentServiceSocial;
+import it.uniroma3.icr.service.impl.TaskService;
 import it.uniroma3.icr.supportControllerMethod.FacebookControllerSupport;
 import it.uniroma3.icr.supportControllerMethod.GoogleControllerSupport;
 import it.uniroma3.icr.supportControllerMethod.InstagramControllerSupport;
 import it.uniroma3.icr.supportControllerMethod.SetSchools;
-import org.codehaus.jackson.map.ObjectMapper;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import org.jinstagram.Instagram;
 import org.jinstagram.entity.users.basicinfo.UserInfo;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.repository.query.Param;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
-
-import java.io.StringWriter;
-
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
-import org.springframework.social.connect.*;
+import org.springframework.social.connect.Connection;
+import org.springframework.social.connect.ConnectionRepository;
 import org.springframework.social.facebook.api.Facebook;
 import org.springframework.social.google.api.Google;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
-import org.springframework.web.bind.annotation.*;
-
-import it.uniroma3.icr.model.Administrator;
-import it.uniroma3.icr.model.Student;
-import it.uniroma3.icr.service.impl.StudentService;
-import it.uniroma3.icr.service.impl.TaskService;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
-
 @Controller
-public class LoginController {
-
+public class LoginController
+{
     @Autowired
     public TaskService taskService;
-
     @Autowired
     public StudentService studentService;
-
     @Autowired
     private StudentServiceSocial userFacadesocial;
-
     @Autowired
     private AdminService adminService;
-
     private ConnectionRepository connectionRepository;
-
     private SetSchools setSchools = new SetSchools();
 
     private Google google;
 
-    private final Logger LOGGER = LoggerFactory.getLogger(this.getClass());
+    private final Logger LOGGER = LoggerFactory.getLogger(getClass());
 
     private FacebookControllerSupport facebookControllerSupport = new FacebookControllerSupport();
 
@@ -79,23 +65,23 @@ public class LoginController {
         this.google = google;
     }
 
-    @RequestMapping(value = "/facebookLogin", method = {RequestMethod.GET, RequestMethod.POST})
-    public String helloFacebook(@RequestParam(value = "daFB", required = false) String daFB, Model model,
-                                @ModelAttribute("social") String social, RedirectAttributes redirectAttributes, HttpSession session) {
 
+    @RequestMapping(value={"/facebookLogin"}, method={org.springframework.web.bind.annotation.RequestMethod.GET, org.springframework.web.bind.annotation.RequestMethod.POST})
+    public String helloFacebook(@RequestParam(value="daFB", required=false) String daFB, Model model, @ModelAttribute("social") String social, RedirectAttributes redirectAttributes, HttpSession session)
+    {
         Facebook facebook = connectionRepository.findPrimaryConnection(Facebook.class).getApi();
         if (daFB == null)
             return "redirect:/login";
-        if (connectionRepository.findPrimaryConnection(Facebook.class) == null)
+        if (connectionRepository.findPrimaryConnection(Facebook.class) == null) {
             return "redirect:/connect/facebook";
-
-        return facebookControllerSupport.facebookLogin(facebook, userFacadesocial, model, social, redirectAttributes,session);
-
+        }
+        return facebookControllerSupport.facebookLogin(facebook, userFacadesocial, model, social, redirectAttributes, session);
     }
 
-
-    @RequestMapping(value = "/instagram", method = RequestMethod.GET)
-    public String helloInstagram(@RequestParam String code, Model model) throws Exception {
+    @RequestMapping(value={"/instagram"}, method={org.springframework.web.bind.annotation.RequestMethod.GET})
+    public String helloInstagram(@RequestParam String code, Model model)
+            throws Exception
+    {
         InstagramJPService instagramObj = new InstagramJPService();
         instagramObj.build();
         Instagram instagram = instagramObj.getInstagram(code);
@@ -106,21 +92,21 @@ public class LoginController {
         return instagramControllerSupport.instagramLogin(model, id, userFacadesocial, userInstagram);
     }
 
-    @RequestMapping(value = "/googleLogin", method = {RequestMethod.GET, RequestMethod.POST})
-    public String helloGoogle(@RequestParam(value = "daGoogle", required = false) String daGoogle, Model model,
-                              @ModelAttribute("social") String social, RedirectAttributes redirectAttributes, HttpSession session) {
 
+    @RequestMapping(value={"/googleLogin"}, method={org.springframework.web.bind.annotation.RequestMethod.GET, org.springframework.web.bind.annotation.RequestMethod.POST})
+    public String helloGoogle(@RequestParam(value="daGoogle", required=false) String daGoogle, Model model, @ModelAttribute("social") String social, RedirectAttributes redirectAttributes, HttpSession session)
+    {
         if (daGoogle == null)
             return "redirect:/login";
-        if (this.connectionRepository.findPrimaryConnection(Google.class) == null)
+        if (connectionRepository.findPrimaryConnection(Google.class) == null) {
             return "redirect:/connect/google";
-
-        return googleControllerSupport.googleLogin(google, connectionRepository, userFacadesocial, model, social, redirectAttributes,session);
-
+        }
+        return googleControllerSupport.googleLogin(google, connectionRepository, userFacadesocial, model, social, redirectAttributes, session);
     }
 
-    @RequestMapping(value = "/login", method = RequestMethod.GET)
-    public String login(ModelMap model, @RequestParam(value = "error", required = false) String error) {
+    @RequestMapping(value={"/login"}, method={org.springframework.web.bind.annotation.RequestMethod.GET})
+    public String login(ModelMap model, @RequestParam(value="error", required=false) String error)
+    {
         if (error != null) {
             model.addAttribute("error", "Username o password non validi");
         }
@@ -129,7 +115,7 @@ public class LoginController {
         return "login";
     }
 
-    @RequestMapping(value = "*/logout", method = RequestMethod.GET)
+    @RequestMapping(value={"*/logout"}, method={org.springframework.web.bind.annotation.RequestMethod.GET})
     public String logoutPage(HttpServletRequest request, HttpServletResponse response) {
         Authentication auth = SecurityContextHolder.getContext().getAuthentication();
         if (auth != null) {
@@ -138,7 +124,7 @@ public class LoginController {
         return "redirect:/login?logout";
     }
 
-    @RequestMapping(value = "/role", method = RequestMethod.GET)
+    @RequestMapping(value={"/role"}, method={org.springframework.web.bind.annotation.RequestMethod.GET})
     public String loginRole(Model model, HttpSession session) {
         Authentication auth = SecurityContextHolder.getContext().getAuthentication();
         String role = auth.getAuthorities().toString();
@@ -146,11 +132,11 @@ public class LoginController {
         Administrator admin = adminService.findAdmin(auth.getName());
         String targetUrl = "";
         if (role.contains("ROLE_USER")) {
-            session.setAttribute("student",student);
+            session.setAttribute("student", student);
             model.addAttribute("student", student);
             targetUrl = "redirect:/user/homeStudent";
         } else if (role.contains("ROLE_ADMIN")) {
-            session.setAttribute("admin",admin);
+            session.setAttribute("admin", admin);
             targetUrl = "redirect:/admin/homeAdmin";
         }
         return targetUrl;
